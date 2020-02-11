2e3df314edbabe17248e9662448555b93a7c4619
Diego Condo <diegocondo10@yahoo.es>
2020-02-10 22:43:31 -0500

    terminado ya con presentacion en la pagina de inicio

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,13 +16,21 @@ const Home = () => {
 
 
     return (
-        <div>
+        <div className="container text-center">
             {
                 /*
                 <button name="imagens3" onClick={handleOnClick}>Imagenes en S3</button>
                 <button name="firebase" onClick={handleOnClick}>Firebase</button>
                 */
             }
+
+            <h1 className="display-3 mt-5">
+                Proyecto y Trabajos de Tendencias Actuales de Programación
+            </h1>
+            <hr className="border border-dark" />
+            <h2 className="display-4">Integrantes:</h2>
+            <h1 className="font-weight-light">Diego Condo</h1>
+            <h1 className="font-weight-light"> Yanina Flores</h1>
         </div>
     )
 }
